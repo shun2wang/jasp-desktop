830c3f09dec54fac5285c7a0e89c75f57d00399f
Shun Wang <shuonwang@gmail.com>
2024-09-08 12:40:20 +0800

    Update translate_json.js

--- .github/script/translate_json.js
+++ .github/script/translate_json.js
@@ -1,5 +1,14 @@
 module.exports = async function ({github, context}, dataJson) {
   console.log(github)
   console.log(context)
-  console.log("dataJson is\n", dataJson)
+    const fs = require('fs/promises');
+
+  fs.readFile('./Resources/Data Sets/index.json')
+    .then((data) => {
+      console.log(data);
+      console.log(JSON.parse(data));
+    })
+    .catch((error) => {
+      console.log(error)
+    });
 }
