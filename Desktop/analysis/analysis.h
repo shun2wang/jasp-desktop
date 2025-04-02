e6b751d7dec4f8821efda9c627b7c0c9abafb412
bruno boutin <b.boutin@uva.nl>
2025-04-02 12:27:25 +0200

    Add override to new analysis function

--- Desktop/analysis/analysis.h
+++ Desktop/analysis/analysis.h
@@ -90,9 +90,9 @@ public:
 	void				setEditOptionsOfPlot(	const std::string & uniqueName, const Json::Value & editOptions);
 	bool				checkAnalysisEntry();
 
-	QVariant			getConstant(const QString& key, const QVariant& defaultValue) const;
-	QVariant			getConstant(const QString& key, const QVariant& defaultValue, const QString& module, const QString& analysis) const;
-	bool				optionLocked(const QString& name) const;
+	QVariant			getConstant(const QString& key, const QVariant& defaultValue) const override;
+	QVariant			getConstant(const QString& key, const QVariant& defaultValue, const QString& module, const QString& analysis) const override;
+	bool				optionLocked(const QString& name) const override;
 
 	const	Json::Value		&	results()			const				{ return _results;							}
 	const	Json::Value		&	userData()			const				{ return _userData;							}
