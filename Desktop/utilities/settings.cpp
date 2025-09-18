f2df86c139720835db6dd5c045c1e7bb4f9d8c5a
Joris Goosen <Joris@JorisGoosen.nl>
2025-09-18 15:52:37 +0200

    autosave every 5 minutes

--- Desktop/utilities/settings.cpp
+++ Desktop/utilities/settings.cpp
@@ -116,7 +116,7 @@ const Settings::Setting Settings::Values[] = {
 	{"startMaximized",				false	},
 	{"storeStateEtc",				false	},
 	{"autoSaveOn",					true	},
-	{"autoSaveInterval",			60	},
+	{"autoSaveInterval",			5*60	},
 };	
 
 QVariant Settings::value(Settings::Type key)
