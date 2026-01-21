5b81a338f9a028f0c55c09b35fba8f825083b17e
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2026-01-21 09:59:13 +0100

    Fix loading of some bundles with altered versionnum

--- Desktop/modules/installedmodules.cpp
+++ Desktop/modules/installedmodules.cpp
@@ -16,13 +16,19 @@ void InstalledModules::parseModuleInfo(const std::string& path, InstalledModules
 	std::ifstream in(path);
 	Json::Value root;
 	Json::Reader().parse(in, root);
-	Json::Value version = root["version"][0];
+	Json::Value version = root["version"];
+
 	std::string strVersion = "";
-	for(int i = 0; i < version.size(); i++) {
-		auto x = version[i];
-		strVersion =  strVersion + version[i].asString() + ".";
+	if(version.isMember("Version"))
+		strVersion = version["Version"].asString();
+	else {
+		version = version[0];
+		for(int i = 0; i < version.size(); i++) {
+			auto x = version[i];
+			strVersion =  strVersion + version[i].asString() + ".";
+		}
+		strVersion.pop_back();
 	}
-	strVersion.pop_back();
 
 	info.name = root["name"].asString();
 	info.version = Version(strVersion);
